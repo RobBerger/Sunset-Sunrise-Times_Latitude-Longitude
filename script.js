2f455a68dd3abd92883b2d621702ba4bbebc129c
berger <robertberger.dev@gmail.com>
2023-01-02 16:59:28 -0500

    example JS for API

---
+++ script.js
@@ -0,0 +1,5 @@
+let baseURL = "https://api.sunrise-sunset.org/json?";
+let myLatitude = 36.72;
+let myLongitude = -4.42;
+
+let myURL = baseURL + "lat=" + myLatitude + "&lng=" + myLongitude + "&date=today";
