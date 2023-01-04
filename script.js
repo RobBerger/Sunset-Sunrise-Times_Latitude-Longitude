74cd8c903ceabea4290149ce4a852b97a85dc8a2
berger <robertberger.dev@gmail.com>
2023-01-04 11:10:38 -0500

    Testing the form function

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 let baseURL = "https://api.sunrise-sunset.org/json?";
-let myLatitude = 36.72;
-let myLongitude = -4.42;
+//let myLatitude = 36.72;
+//let myLongitude = -4.42;
 
 async function generateSunTimes() {
+    let myLatitude = document.getElementById('lat').value;
+    let myLongitude = document.getElementById('lng').value;
     let myURL = await fetch(baseURL + "lat=" + myLatitude + "&lng=" + myLongitude + "&date=today")
         .then(response => response.json());
     let sunRiseTime = myURL.results.sunrise;
     let sunSetTime = myURL.results.sunset;
+    
+    console.log(myURL);
+    console.log(myLatitude);
+    console.log(myLongitude);
     console.log(sunRiseTime);
     console.log(sunSetTime);
 }
