bdf1e39113e7a8c38c75255b99b72ec896722d82
berger <robertberger.dev@gmail.com>
2023-01-04 10:12:36 -0500

    attached button to async function

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// let baseURL = "https://api.sunrise-sunset.org/json?";
-// let myLatitude = 36.72;
-// let myLongitude = -4.42;
+let baseURL = "https://api.sunrise-sunset.org/json?";
+let myLatitude = 36.72;
+let myLongitude = -4.42;
 
-// async function generateSunTimes() {
-//     let myURL = await fetch(baseURL + "lat=" + myLatitude + "&lng=" + myLongitude + "&date=today")
-//         .then(response => response.json());
-//     let sunRiseTime = myURL.results.sunrise;
-//     let sunSetTime = myURL.results.sunset;
-//     console.log(sunRiseTime);
-//     console.log(sunSetTime);
-// }
+async function generateSunTimes() {
+    let myURL = await fetch(baseURL + "lat=" + myLatitude + "&lng=" + myLongitude + "&date=today")
+        .then(response => response.json());
+    let sunRiseTime = myURL.results.sunrise;
+    let sunSetTime = myURL.results.sunset;
+    console.log(sunRiseTime);
+    console.log(sunSetTime);
+}
 
 var btn = document.getElementById('solarButton');
 
 btn.addEventListener('click', () => {
-    console.log('Test');
+    generateSunTimes();
 });
